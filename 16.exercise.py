728e77f9f02cf502d4e79d852255cb5b966e55a3
devops-hemant <devopshemant@gmail.com>
2026-02-08 12:29:35 +0000

    functions

---
+++ 16.exercise.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# services = ["AWS", "VM", "AKS", "WebApp"]
+
+# # Print only AKS services
+
+# for service in services:
+#     if service == "AKS":
+#         print(service)  
+
+
+# resource = {
+#     "name": "web-prod",
+#     "status": "Stopped"
+# }
+
+# # If status is Stopped → print alert
+
+# if resource["status"]  == "Stopped":
+#     print("ALERT: Resource is stopped") 
+
+
+# services = ["AWS", "AKS", "VM", "AKS", "WebApp"]
+
+# # Print AKS count
+
+# ask_count = 0
+
+# for service in services:
+#     if service == "AKS":
+#         ask_count += 1
+
+# print (ask_count)
