dff8ed664535a73adb8d7173a36dd13ae139112f
Hemant Sharma <hemant@Hemants-MacBook-Air.local>
2026-02-14 14:05:11 +0530

    test

--- main.py
+++ main.py
@@ -1,72 +1,19 @@
 #!/usr/bin/python3
 
-'''
-🔹 Task 1
-Read a file and:
-Print only lines containing "INFO"
-'''
+# import json
 
-with open("app.log", "r") as file:
-    for line in file:
-        if "INFO" in line:
-            print(line.strip())
+# with open("resources.json", "r") as file:
+#     resources = json.load(file)
+
+# for r in resources:
+#     print(r["name"], "->", r["status"])
 
 
-'''
-🔹 Task 2
-Create a file summary.txt:
-Write total number of "ERROR" lines from app.log
-'''
+import json
 
-error_count = 0
-with open("app.log", "r") as file:
-    for line in file:
-        if "ERROR" in line:
-            error_count += 1
+with open("resources.json", "r") as file:
+    resources = json.load(file)
 
-with open("summary.txt", "w+") as file:
-    file.write(f"Total number of Errors: {error_count}")
-    
-    file.seek(0)
-    content=file.read()
-    print(content)
-
-
-
-'''
-🔹 Task 3 (Bonus)
-If error count > 5:
-Write "CRITICAL" in report
-'''
-def err_count(logfile):
-    error_count = 0
-    try:
-        with open(logfile, "r") as file:
-            for line in file:
-                if "ERROR" in line:
-                    error_count += 1
-        return error_count
-    except FileNotFoundError:
-        print("❌ file not found")
-        return -1
-
-
-def report(error_count, status):
-    with open("summary.txt", "w") as file:
-        file.write(f"Total errors: {error_count}\n")
-        file.write(f"Status: {status}\n")
-
-    with open("summary.txt", "r") as file:
-        print(file.read())
-
-
-logcheck = err_count("app.log")
-
-if logcheck == -1:
-    report(logcheck, "Log file missing")
-elif logcheck > 5:
-    report(logcheck, "Critical")
-elif logcheck >= 3:
-    report(logcheck, "High")
-else:
-    report(logcheck, "Low")
+for r in resources:
+    if r["status"] != "Running":
+        print("⚠ Alert:", r["name"], "is", r["status"])
